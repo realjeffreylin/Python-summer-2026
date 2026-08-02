dc41cd86029fd3514886666a38c996e5b1afd3e4
realjeffreylin <realjeffreylin@gmail.com>
2026-08-01 21:51:14 -0400

    modify file1 and add day05
    modify file1 and add day05

---
+++ Day05_02.py
@@ -0,0 +1,9 @@
+def print_Jeff ():
+    text = 'I could do every python learning in 30 days Imma trynna stay consistent'
+    print(text)
+    print(text)
+    print(text)
+
+print_Jeff()
+print_Jeff()
+print_Jeff()
--- file1.py
+++ file1.py
@@ -1,1 +1,3 @@
 print("hello 1")
+print("hello world")
+print(len("hello world"))
