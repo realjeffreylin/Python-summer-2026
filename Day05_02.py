f4c43257e658e3efd9e5ea6d727f58a9ef1d2f8e
realjeffreylin <realjeffreylin@gmail.com>
2026-08-01 22:01:43 -0400

    add new command

--- Day05_02.py
+++ Day05_02.py
@@ -7,3 +7,13 @@ def print_Jeff ():
 print_Jeff()
 print_Jeff()
 print_Jeff()
+
+# Single line comment
+letter = 'P'                # A string could be a single character or a bunch of texts
+print(letter)               # P
+print(len(letter))          # 1
+greeting = 'Hello, World!'  # String could be  a single or double quote,"Hello, World!"
+print(greeting)             # Hello, World!
+print(len(greeting))        # 13
+sentence = "I hope you are enjoying 30 days of python challenge"
+print(sentence)
