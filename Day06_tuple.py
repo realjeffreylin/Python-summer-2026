b40b429558bdbd1a5f166ce2510eee5be7453552
realjeffreylin <realjeffreylin@gmail.com>
2026-08-06 18:47:28 -0400

    06 commit 1

---
+++ Day06_tuple.py
@@ -0,0 +1,17 @@
+#How to build a list group 
+Categories = list()
+print(Categories)
+
+Category = []
+print(len(Category)) #It shows that we didnt fill the list between in our braket
+
+#Finding a length that are included in our list 
+
+Vehicules = ['Car','trucks','Boat','Airplane']
+print('Vehicules:','Vehicules')
+print('Number of vehicules:',print(len(Vehicules)))
+
+Car_brands = ['Honda','Hyundai','Kia','Chrysler','Dodge']
+print('Car_brands:',Car_brands)
+print('The number of Car_brands:','print(len(Car_brands))')
+
